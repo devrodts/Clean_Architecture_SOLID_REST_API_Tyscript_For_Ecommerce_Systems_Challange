b0480c7d26f2416c6a3a896094daaffa9ccc0f87
rod <dev_rodts@outlook.com>
2025-02-22 15:43:31 -0300

    "feat: add typeorm product entity"

---
+++ src/core/infra/database/entities/product/entities/typeorm-product.entity.ts
@@ -0,0 +1,26 @@
+
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('products')
+export class TypeOrmProductEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  name: string;
+
+  @Column({ type: 'text' })
+  description: string;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  price: number;
+
+  @Column({ type: 'int' })
+  stock: number;
+
+  @CreateDateColumn({ type: 'timestamptz' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamptz' })
+  updatedAt: Date;
+}
