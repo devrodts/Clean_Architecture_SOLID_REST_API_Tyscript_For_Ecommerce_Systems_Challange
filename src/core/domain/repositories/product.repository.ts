7a15137629bf66bbdb5cd75477138b4432f3681e
rod <dev_rodts@outlook.com>
2025-02-27 11:20:02 -0300

    "refactor: product entity"

--- src/core/domain/repositories/product.repository.ts
+++ src/core/domain/repositories/product.repository.ts
@@ -3,7 +3,7 @@ import { ProductEntity } from "../entities/product/product.entity";
 export interface ProductRepository {
     findById(id: string): Promise<ProductEntity | null>;
     findAll(): Promise<ProductEntity[]>;
-    save(product: ProductEntity): Promise<void>;
+    create(product: ProductEntity): Promise<void>;
     update(product: ProductEntity): Promise<void>;
     delete(id: string): Promise<void>;
   }
