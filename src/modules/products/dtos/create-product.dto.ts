a41b4960bd8328ac6f02ddc62801b63072909a34
rod <dev_rodts@outlook.com>
2025-02-27 11:25:25 -0300

    "refactor: create product request"

--- src/modules/products/dtos/create-product.dto.ts
+++ src/modules/products/dtos/create-product.dto.ts
@@ -1,19 +1,30 @@
 // src/modules/products/dto/create-product.request.ts
-import { IsString, Length, IsNumber, Min, IsInt } from 'class-validator';
+import { IsString, Length, IsNumber, Min, IsInt, IsDate } from 'class-validator';
+import { PrimaryGeneratedColumn } from 'typeorm';
 
 export class CreateProductRequest {
 
   constructor(
+    id: string,
     name: string,
     description: string,
     price: number,
     stock: number,
+    createdAt: Date,
+    updatedAt: Date,
   ) {
+    this.id = id;
     this.name = name;
     this.description = description;
     this.price = price;
     this.stock = stock;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
   }
+
+  @PrimaryGeneratedColumn('uuid')
+  @IsString()
+  id: string; 
 
   @IsString()
   @Length(3, 100)
@@ -30,4 +41,10 @@ export class CreateProductRequest {
   @IsInt()
   @Min(0)
   stock: number;
+
+  @IsDate()
+  createdAt: Date;
+
+  @IsDate()
+  updatedAt: Date;
 }
