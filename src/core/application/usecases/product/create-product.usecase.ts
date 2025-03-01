d5cf96907b96e2381ed983fdeed2be85fd9aa26b
rod <dev_rodts@outlook.com>
2025-03-01 09:38:46 -0300

    "refactor: refactor create product use case to use product service"

--- src/core/application/usecases/product/create-product.usecase.ts
+++ src/core/application/usecases/product/create-product.usecase.ts
@@ -1,18 +1,18 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { ProductEntity } from '../../../domain/entities/product/product.entity';
 import { CreateProductRequest } from 'src/modules/products/dtos/create-product.dto';
-import { ProductsService } from '../../services/product/products.service';
+import { ProductsService } from 'src/core/application/services/product/products.service';
 
 @Injectable()
 export class CreateProductUseCase {
 
   constructor(
-    @Inject('ProductRepository')
+    @Inject('ProductService')
     private readonly productService: ProductsService,
   ) {}
 
   async execute(product: CreateProductRequest): Promise<ProductEntity | void> {
-    const newProduct = await this.productService.create(product);
+    const newProduct = await this.productService.createProduct(product);
     return newProduct;
   }
 }
