04e2186c4cf34492dc19a83b2a099c27cb97e6e1
rod <dev_rodts@outlook.com>
2025-03-01 08:39:02 -0300

    "refactor: cleaning create-product use case"

--- src/core/application/usecases/product/create-product.usecase.ts
+++ src/core/application/usecases/product/create-product.usecase.ts
@@ -1,16 +1,13 @@
-// src/core/application/usecases/create-product.usecase.ts
 import { Injectable, Inject } from '@nestjs/common';
 import { ProductEntity } from '../../../domain/entities/product/product.entity';
-import { ProductRepository } from '../../../domain/repositories/product.repository';
 import { CreateProductRequest } from 'src/modules/products/dtos/create-product.dto';
-import { TypeOrmProductRepository } from 'src/core/infra/repositories/product/typeorm-product.repository';
 import { ProductsService } from '../../services/product/products.service';
 
 @Injectable()
 export class CreateProductUseCase {
+
   constructor(
     @Inject('ProductRepository')
-    private readonly productRepository: TypeOrmProductRepository,
     private readonly productService: ProductsService,
   ) {}
 
