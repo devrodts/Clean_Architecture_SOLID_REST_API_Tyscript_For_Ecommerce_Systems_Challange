a2c3fa9eb944445941e272c763443666418404ac
rod <dev_rodts@outlook.com>
2025-03-01 09:42:57 -0300

    "chore: remove some files"

--- src/core/domain/repositories/product.repository.ts
+++
@@ -1,9 +0,0 @@
-import { ProductEntity } from "../entities/product/product.entity";
-
-export interface ProductRepository {
-    findById(id: string): Promise<ProductEntity | null>;
-    findAll(): Promise<ProductEntity[]>;
-    create(product: ProductEntity): Promise<void>;
-    update(product: ProductEntity): Promise<void>;
-    delete(id: string): Promise<void>;
-  }
